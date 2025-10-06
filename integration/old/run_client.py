02bcd7b48fdd4ef394bb9f6ea8b34f0c2b80cfc4
Aniket Rattan <aniketrattan2005@gmail.com>
2025-10-06 22:58:35 +1030

    removed old files

--- integration/old/run_client.py
+++
@@ -1,7 +0,0 @@
-import asyncio
-from client import Client
-
-if __name__ == "__main__":
-
-    client = Client()
-    asyncio.run(client.run_client())
--- integration/old/run_server.py
+++
@@ -1,5 +0,0 @@
-import asyncio
-from server import start_server
-
-if __name__ == "__main__":
-    asyncio.run(start_server())
